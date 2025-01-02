5ed37517710dd6c189f872dd3bbe9418f0d83b78
Aurora0043 <940403army@gmail.com>
2025-01-02 20:32:43 +0800

    0

--- app/src/main/java/com/example/week17/MainActivity.kt
+++ app/src/main/java/com/example/week17/MainActivity.kt
@@ -4,13 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.week17.ui.theme.Week17Theme
 
 class MainActivity : ComponentActivity() {
@@ -24,24 +45,61 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    DrawCircle()
                 }
             }
         }
     }
 }
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            Text(text = "多指觸控Compose實例",
+                fontFamily = FontFamily(Font(R.font.finger)),
+                fontSize = 25.sp,
+                color = Color.Blue)
+            Image(
+                painter = painterResource(id = R.drawable.hand),
+                contentDescription = "手掌圖片",
+                alpha = 0.7f,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(Color.Blue)
+            )
+        }
+        Text(text = "作者：張綺庭",
+            fontFamily = FontFamily(Font(R.font.finger)),
+            fontSize = 25.sp,
+            color = Color.Black)
+    }
 }
 
-@Preview(showBackground = true)
+
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun GreetingPreview() {
-    Week17Theme {
-        Greeting("Android")
-    }
+fun DrawCircle() {
+    var X = remember { mutableStateListOf(0f) }
+    var Y = remember { mutableStateListOf(0f) }
+    var Fingers by remember { mutableStateOf(0) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInteropFilter { event ->
+                Fingers = event.getPointerCount()
+                X.clear()
+                Y.clear()
+                for (i in 0..Fingers - 1) {
+                    X.add(event.getX(i))
+                    Y.add(event.getY(i))
+                }
+                true
+            }
+    )
 }
